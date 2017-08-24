a607bba4172e3ab39b050013e8a037dcce382de8
aarizvi <aarizv@gmail.com>
2017-08-24 13:38:28 -0400

    Added full chunk with SE object, removed pheno parsing to own function addCov

---
+++ addCov_v0.1.R
@@ -0,0 +1,53 @@
+
+
+############################
+## load phenotype data
+############################
+
+pheno <- fread("~/GoogleDrive/OSU_PHD/mhcproject/BMT_PHENOTYPE_FILE_QC_allclinical_5_5_17.txt")
+
+#######################
+## clean phenotype data
+#######################
+
+# remove SI.rsxxxx I.rsxxxx ST, T.rsxxxxxx columns
+pheno <- pheno %>% 
+        select(-c(grep("I.r", colnames(pheno)),
+                  grep("SI.", colnames(pheno)),
+                  grep("ST.", colnames(pheno)),
+                  grep("T.", colnames(pheno))))
+
+
+pheno <- pheno %>% 
+        select(-PC1.AA.c1:-PC10.HIS.c1)
+
+pheno$IID.c1.c2 <- NA 
+pheno$IID.c1.c2[!is.na(pheno$IID.EA.c1)] <- pheno$IID.EA.c1[!is.na(pheno$IID.EA.c1)]
+pheno$IID.c1.c2[!is.na(pheno$IID.EA.c2)] <- pheno$IID.EA.c2[!is.na(pheno$IID.EA.c2)]
+
+head(pheno)
+
+# change those 3 patients IDs that are swapped 
+pheno$IID.c1.c2 <- pheno$IID.c1.c2 %>% dplyr::recode(`G_BMT_OmniExp_BMT-125_0085017851`="G_BMT_OmniExp_BMT-125_0085017823",
+                                                     `G_BMT_OmniExp_BMT-152_0085017639`="G_BMT_OmniExp_BMT-152_1023993633",
+                                                     `G_BMT_OmniExp_BMT-147_0105388831`="G_BMT_OmniExp_BMT-119_0074380675") 
+
+# create dummy columns for trm covariates
+
+trm.cov <- pheno %>%
+        select(IID.c1.c2, intxsurv_1Y, trm_overall, age, bmi_cat, graftype) %>%
+        mutate(bmiOBS=ifelse(bmi_cat=="obese", 1, 0),
+               bmiOVWT=ifelse(bmi_cat=="overweight", 1, 0),
+               PBlood=ifelse(graftype=="Peripheral blood", 1, 0)) %>%
+        select(-bmi_cat, -graftype) %>%
+        rename(sampleID=IID.c1.c2) %>%
+        data.table %>%
+        na.omit
+
+length(colData(se)$sampleID)
+
+se.sub <- se[,colnames(se) %in% trm.cov$sampleID]
+
+
+
+data.frame(colData(se)) %>% left_join(trm.cov) %>% na.omit %>% nrow
--- readImputed_v0.1.R
+++ readImputed_v0.1.R
@@ -24,16 +24,17 @@ input.files <- c(paste0(path, "/", "BMT093013_forImpute.chr7-55000000-60000000.i
 chromosome <- 7
 
 input.type <- "IMPUTE2"
-input.dosage=F
+input.dosage <- F
 
-#read.imputed <- function(input.files, chromosome, input.type, input.dosage, select.snps=NULL, ...){
-gdsfile <- tempfile()
-snpfile <- tempfile()
-scanfile <- tempfile()
+readImpute <- function(input.files, info.file, chromosome, input.type, input.dosage, select.snps=NULL, ...){
 
-if(is.null(chromosome)){
-        chromosome <- NA
-}
+# gdsfile <- tempfile()
+# snpfile <- tempfile()
+# scanfile <- tempfile()
+# 
+# if(is.null(chromosome)){
+#         chromosome <- NA
+# }
 
 imputedDosageFile(input.files=input.files,
                   filename=gdsfile,
@@ -56,6 +57,7 @@ genoData <- GenotypeData(gds,
 dat <- list()
 dat$genotypes <- getGenotype(genoData)
 dim(dat$genotypes)
+dimnames(dat$genotypes) <- list(dat.gr$rsID, dat$fam$sampleID)
 
 
 
@@ -66,23 +68,26 @@ dat$map <- genoData@snpAnnot@data[,-1]
 dat$fam <- genoData@scanAnnot@data
 
 
-
 # read in info table
 infoFile <- read.table("BMT093013_forImpute.chr7-55000000-60000000.impute2_info", header=T)
 # only grab columns of interest
-infoFile <- infoFile[,c("rs_id", "exp_freq_a1", "info", "certainty")]
+infoFile <- infoFile[,c("snp_id", "rs_id", "exp_freq_a1", "info", "certainty")]
 # need to be able to generalize the following renaming in function
-infoFile <- infoFile %>% rename(rsID=rs_id)
+infoFile <- infoFile %>% rename(rsID=rs_id, snp=snp_id)
 # merge 'map' file with info file
 dat$map <- dat$map %>% left_join(infoFile)
+
+# create 'end' position -- since these are SNPs they are the same 
 dat$map$end <- dat$map$position 
+
+# rename position to start so we can convert into GRanges object
 dat$map <- dat$map %>% rename(start=position, chr=chromosome) 
 
 # create GRanges object because needed for rowRanges in se
 dat.gr <- makeGRangesFromDataFrame(dat$map, keep.extra.columns = T)
 
 
-
+# parse sample file ... but might not do this
 dat$fam <- dat$fam[,"sampleID"]
 dat$fam <- sapply(strsplit(dat$fam, " "), "[", 2)
 dat$fam <- DataFrame(sampleID=dat$fam)
@@ -95,87 +100,21 @@ dat$fam <- DataFrame(sampleID=dat$fam)
 # }else if(input.type=="MaCH"){
 #         snpid <- dat$map$snp
 # }
-# rownames(dat$genotypes) <- rownames(dat$map) <- snpid
-# colnames(dat$genotypes) <- rownames(dat$fam) <- dat$fam$sampleID
-# 
+
+# create SummarizedExperiment object
+
+se.full <- SummarizedExperiment(assays=list("chr7-55000000-60000000.impute2"=dat$genotypes),
+                                colData=dat$fam,
+                                rowRanges=dat.gr)
 
 
-#####################        
-# subset for testing
-#####################
-
-dat.sub <- list()
-dat.sub$genotypes <- dat$genotypes[1:1000,1:6805]
-dat.sub$rowData <- dat.gr[1:1000,]
-dat.sub$colData <- dat$fam
-dimnames(dat.sub$genotypes) <- list(dat.sub$rowData$rsID, dat.sub$colData$sampleID)
-head(dat$fam)
-
-
-se <- SummarizedExperiment(assays=list("chr7-55000000-60000000.impute2"=dat.sub$genotypes),
-                      colData=dat.sub$colData,
-                      rowRanges=dat.sub$rowData)
-
-############################
-## load phenotype data
-############################
-
-pheno <- fread("~/GoogleDrive/OSU_PHD/mhcproject/BMT_PHENOTYPE_FILE_QC_allclinical_5_5_17.txt")
-
-#######################
-## clean phenotype data
-#######################
-
-# remove SI.rsxxxx I.rsxxxx ST, T.rsxxxxxx columns
-pheno <- pheno %>% 
-        select(-c(grep("I.r", colnames(pheno)),
-                  grep("SI.", colnames(pheno)),
-                  grep("ST.", colnames(pheno)),
-                  grep("T.", colnames(pheno))))
-
-
-pheno <- pheno %>% 
-        select(-PC1.AA.c1:-PC10.HIS.c1)
-
-pheno$IID.c1.c2 <- NA 
-pheno$IID.c1.c2[!is.na(pheno$IID.EA.c1)] <- pheno$IID.EA.c1[!is.na(pheno$IID.EA.c1)]
-pheno$IID.c1.c2[!is.na(pheno$IID.EA.c2)] <- pheno$IID.EA.c2[!is.na(pheno$IID.EA.c2)]
-
-head(pheno)
-
-# change those 3 patients IDs that are swapped 
-pheno$IID.c1.c2 <- pheno$IID.c1.c2 %>% dplyr::recode(`G_BMT_OmniExp_BMT-125_0085017851`="G_BMT_OmniExp_BMT-125_0085017823",
-                                              `G_BMT_OmniExp_BMT-152_0085017639`="G_BMT_OmniExp_BMT-152_1023993633",
-                                              `G_BMT_OmniExp_BMT-147_0105388831`="G_BMT_OmniExp_BMT-119_0074380675") 
-
-# create dummy columns for trm covariates
-
-trm.cov <- pheno %>%
-        select(IID.c1.c2, intxsurv_1Y, trm_overall, age, bmi_cat, graftype) %>%
-        mutate(bmiOBS=ifelse(bmi_cat=="obese", 1, 0),
-               bmiOVWT=ifelse(bmi_cat=="overweight", 1, 0),
-               PBlood=ifelse(graftype=="Peripheral blood", 1, 0)) %>%
-        select(-bmi_cat, -graftype) %>%
-        rename(sampleID=IID.c1.c2) %>%
-        data.table %>%
-        na.omit
-
-length(colData(se)$sampleID)
-
-se.sub <- se[,colnames(se) %in% trm.cov$sampleID]
-
-
-
-data.frame(colData(se)) %>% left_join(trm.cov) %>% na.omit %>% nrow
-
-
-# if(!is.null(select.snps)){
-#         se <- se[select.snps,]
-# }
-# close(gds)
-# unlink(c(gdsfile, snpfile, scanfile))
-# return(se)
-#}
+if(!is.null(select.snps)){
+        se <- se[select.snps,]
+}
+close(gds)
+unlink(c(gdsfile, snpfile, scanfile))
+return(se)
+}
 
 # library(survival)
 
