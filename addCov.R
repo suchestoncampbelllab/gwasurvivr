2355ef2603cbef92ec37de4dee563581d29f8789
aarizvi <aarizv@gmail.com>
2017-09-19 13:23:43 -0400

    Fixed covariate merging issue

---
+++ addCov.R
@@ -0,0 +1,7 @@
+addCov <- function(se, covFile){
+        se <- se[,colnames(se) %in% covFile[[1]]]
+        colnames(covFile)[1] <- "ID_2"
+        colData(se) <- DataFrame(plyr::join(data.frame(colData(se)), covFile))
+        colnames(se) <- colData(se)$ID_2
+        return(se)
+}
