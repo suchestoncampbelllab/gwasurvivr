f19ab8b414aec047ebb5db157cee20342d62190d
aarizvi <aarizv@gmail.com>
2017-09-19 13:24:15 -0400

    Added time event covariate as inputs to function

--- coxSurv.R
+++ coxSurv.R
@@ -1,60 +1,50 @@
-## Survival Function
-library(survival)
-
-
-coxSurv <- function(se, ncore=1){
+coxSurv <- function(se, time, event, covariates){
         library(survival)
-        .env <- environment()
-
+        #.env <- environment()
+        colData(se) <- colData(se)[,c(time, event, covariates)]
+        
         # maybe add operability with different assays
         formula <- paste0("Surv(time=",
-                      names(colData(se))[2],
-                      ", event=",
-                      names(colData(se))[3],
-                      ") ~ input.genotype + ",
-                      paste(names(colData(se)[4:length(names(colData(se)))]), collapse=" + "))
+                          time,
+                          ", event=",
+                          event,
+                          ") ~ input.genotype + ",
+                          paste(covariates, collapse=" + "))
         
         survFit <- function(input.genotype){
                 stat <- tryCatch(coxph(as.formula(formula), data=data.frame(colData(se))),
                                  warning=function(warn)NA,
                                  error=function(err)NA
-                                 )
+                )
                 if(any(is.na(stat))){
-                        rep(NA, 6)
+                        rep(NA, 7)
                 }else{
                         m <- summary(stat)
-                        c(m$coef[1,], n=m$n)
+                        c(m$coef[1,], n=m$n, nevents=m$nevent)
                 }
         }
         
-        #parallel when ncore>1
-        if(ncore==1){
-                sres <- t( apply( assay(se), 1, function(i) survFit(i)  ))
-        }else if(ncore>1){
-                cl <- makeCluster(ncore, type="SOCK")
-                clusterExport(cl, c(colData(se),"formula", "survFit"), envir=.env)
-                clusterEvalQ(cl, library(survival))
-                sres <- t(parApply(cl, assay(se), 1, function(i) survFit(i)) )
-                stopCluster(cl)
-        }else{
-                stop("ncore should be >= 1")
-        }
+        sres <- t( apply( assay(se), 1, function(i) survFit(i)  ))
         
         # change survival result matrix column names
-        colnames(sres) <- c("coef", "exp.coef","se.coef","z", "p-value", "n")
+        colnames(sres) <- c("coef", "exp.coef","se.coef","z", "p.value", "n", "nevents")
+        
         # remove rownames so we don't have a duplicated rownames issue
         rownames(sres) <- NULL
+        
+        sres <- data.frame(sres)
+        
+        sres <- cbind(sres,
+                      lb=exp(sres$coef-sres$se.coef),
+                      ub=exp(sres$coef+sres$se.coef))
+        
+        
+        sres <- sres[,c("coef", "se.coef", "exp.coef", "lb", "ub", "z", "p.value", "n", "nevents")]
         
         # add survival results into summarizedexperiment object
         mcols(rowRanges(se)) <- cbind(mcols(rowRanges(se)), data.frame(sres))
         
         metadata(se) <- list(formula=formula)
         ## we also may want to keep the formula info
-        
         return(se)
-        
-        
 }
-
-library(microbenchmark)
-microbenchmark(se.surv <- coxSurv(se), times=1)
