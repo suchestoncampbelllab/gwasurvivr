7c8fe80c88b3eb535f1cea11106e09472efd9e26
aarizvi <aarizv@gmail.com>
2017-09-21 14:28:47 -0400

    fixes the read table

--- addCov.R
+++ addCov.R
@@ -1,6 +1,6 @@
 addCov <- function(se, covfile){
         library(SummarizedExperiment)
-        covfile <- read.table(covfile, header=T, sep="\t")
+        covfile <- read.table(covfile, header=T, sep="\t", stringsAsFactors=F)
         se <- se[,colnames(se) %in% covfile[[1]]]
         colnames(covfile)[1] <- "ID_2"
         colData(se) <- DataFrame(plyr::join(data.frame(colData(se)), covfile))
