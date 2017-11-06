ae57a25d118902b91307223267dbe8a636a3df40
aarizvi <aarizv@gmail.com>
2017-11-06 14:37:02 -0500

    moved into R folder

--- addCov.R
+++
@@ -1,9 +0,0 @@
-addCov <- function(se, covfile){
-        library(SummarizedExperiment)
-        covfile <- read.table(covfile, header=T, sep="\t", stringsAsFactors=F)
-        se <- se[,colnames(se) %in% covfile[[1]]]
-        colnames(covfile)[1] <- "ID_2"
-        colData(se) <- DataFrame(plyr::join(data.frame(colData(se)), covfile))
-        colnames(se) <- colData(se)$ID_2
-        return(se)
-}
