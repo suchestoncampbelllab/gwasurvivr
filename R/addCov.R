13c76b888dbf1be157fce0cd66ff62f69c885cd9
aarizvi <aarizv@gmail.com>
2017-12-01 10:45:47 -0500

    updated vignette, addCov.R with parameters and DESCRIPTION file

--- R/addCov.R
+++ R/addCov.R
@@ -1,5 +1,10 @@
+#'
+#'
+#'
+#'
+#'
+
 addCov <- function(se, covfile){
-        library(SummarizedExperiment)
         covfile <- read.table(covfile, header=T, sep="\t", stringsAsFactors=F)
         se <- se[,colnames(se) %in% covfile[[1]]]
         colnames(covfile)[1] <- "ID_2"
