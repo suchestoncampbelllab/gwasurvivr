016bbad18d5157d5b6f857cb28c21fc10a1e429d
aarizvi <aarizv@gmail.com>
2018-03-21 22:24:11 -0400

    cleaned up impute2

--- R/impute2CoxSurv.R
+++ R/impute2CoxSurv.R
@@ -133,7 +133,7 @@ impute2CoxSurv <- function(impute.file,
         if(!is.null(info.filter)){
                 genotypes <- genotypes[complete.cases(snp),]
                 snp <- snp[complete.cases(snp),]
-                genotypes <- genotypes[snpw$info>info.filter,]
+                genotypes <- genotypes[snp$info>info.filter,]
                 snp <- snp[snp$info>info.filter,]
                 rm.snps <- rbind(rm.snps, snp[!snp$info>info.filter, c("snpid", "rsid", "exp_freq_A1", "info")])
         }
