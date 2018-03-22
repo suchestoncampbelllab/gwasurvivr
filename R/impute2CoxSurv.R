b2e2dcd26af1b0a93ff2a2532749df0522e0434c
aarizvi <aarizv@gmail.com>
2018-03-21 21:39:42 -0400

    cleaned up impute2

--- R/impute2CoxSurv.R
+++ R/impute2CoxSurv.R
@@ -133,6 +133,7 @@ impute2CoxSurv <- function(impute.file,
         if(!is.null(info.filter)){
                 genotypes <- genotypes[snp$info>info.filter,]
                 rm.snps <- rbind(rm.snps, snp[!snp$info>info.filter, c("snpid", "rsid", "exp_freq_A1", "info")])
+                rm.snps <- rm.snps %>% na.omit
         }
         write.table(rm.snps, 
                     paste0(out.file, ".snps_removed"),
