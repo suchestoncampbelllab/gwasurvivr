8767c2a6439b84d33a37e9de52c42e52eb017ff9
aarizvi <aarizv@gmail.com>
2018-03-21 22:22:34 -0400

    cleaned up impute2

--- R/impute2CoxSurv.R
+++ R/impute2CoxSurv.R
@@ -131,10 +131,11 @@ impute2CoxSurv <- function(impute.file,
                     }
         }
         if(!is.null(info.filter)){
-                genotypes <- genotypes[snp$info>info.filter,]
+                genotypes <- genotypes[complete.cases(snp),]
+                snp <- snp[complete.cases(snp),]
+                genotypes <- genotypes[snpw$info>info.filter,]
                 snp <- snp[snp$info>info.filter,]
                 rm.snps <- rbind(rm.snps, snp[!snp$info>info.filter, c("snpid", "rsid", "exp_freq_A1", "info")])
-                rm.snps <- rm.snps[complete.cases(rm.snps),]
         }
         write.table(rm.snps, 
                     paste0(out.file, ".snps_removed"),
