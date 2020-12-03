c2e79a08ba69ea5025f1665d8c17cc92267ac25c
Mark Platts <markplatts23@gmail.com>
2020-12-02 20:17:28 -0500

    Added tests using testthat to ensure that refactoring does not change the result files being output from all xCoxSurv functions.

---
+++ tests/testthat/test-check_michigan_results_unchanged.R
@@ -0,0 +1,42 @@
+local_edition(3) 
+results_file_name <- "example_results"
+
+
+vcf.file <- system.file(package="gwasurvivr",
+                         "extdata",
+                      "michigan.chr14.dose.vcf.gz")
+pheno.fl <- system.file(package="gwasurvivr",
+                      "extdata",
+                   "simulated_pheno.txt")
+pheno.file <- read.table(pheno.fl, 
+                       sep=" ",
+                       header=TRUE,
+                       stringsAsFactors = FALSE)
+pheno.file$SexFemale <- ifelse(pheno.file$sex=="female", 1L, 0L)
+sample.ids <- pheno.file[pheno.file$group=="experimental",]$ID_2
+
+michiganCoxSurv(vcf.file=vcf.file,
+            covariate.file=pheno.file,
+            id.column="ID_2",
+            sample.ids=sample.ids,
+            time.to.event="time",
+            event="event",
+            covariates=c("age", "SexFemale", "DrugTxYes"),
+            inter.term=NULL,
+            print.covs="only",
+            out.file=results_file_name,
+            r2.filter=0.3,
+            maf.filter=0.005,
+            chunk.size=50,
+            verbose=FALSE,
+            clusterObj=NULL)
+
+results_file <- read.table(paste0(results_file_name, ".coxph"), sep="\t", header=TRUE, stringsAsFactors = FALSE)
+
+test_that("check gds example results not changed", {
+   skip_on_cran()
+   expect_snapshot_output(results_file)
+})
+
+file.remove(paste0(results_file_name, ".coxph"))
+file.remove(paste0(results_file_name, ".snps_removed"))
