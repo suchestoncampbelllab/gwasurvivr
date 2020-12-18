4e3f6779b96589ca095bde0c5e3d0e9fe77ada9d
Mark Platts <markplatts23@gmail.com>
2020-12-17 21:43:52 -0500

    Moved closing_messages and create_cluster into same function as main function because they are only small helper functions.

--- R/coxSurv.R
+++ R/coxSurv.R
@@ -32,6 +32,44 @@ coxSurv <- function(cox_surv) {
   
 }
 
+
+
+#create cluster object depending on user pref or OS type,
+# also create option to input number of cores
+create_cluster_obj <- function(clusterObj) {
+  if(!is.null(clusterObj)){
+    cl <- clusterObj
+  }else if(.Platform$OS.type == "unix") {
+    cl <- makePSOCKcluster(getOption("gwasurvivr.cores", 2L))
+  } else {
+    cl <- makeCluster(getOption("gwasurvivr.cores", 2L))
+  }
+  return(cl)
+}
+
+
+
+closing_messages <- function(snps_removed, snps_analyzed, out.file){
+  
+  message("Analysis completed on ",
+          format(Sys.time(), "%Y-%m-%d"),
+          " at ",
+          format(Sys.time(), "%H:%M:%S"))
+  
+  message(snps_removed, " SNPs were removed from the analysis for ",
+          "not meeting the threshold criteria.")
+  
+  message("List of removed SNPs can be found in ", 
+          paste0(out.file, ".snps_removed"))
+  
+  message(snps_analyzed, " SNPs were analyzed in total")
+  
+  message("The survival output can be found at ",
+          paste0(out.file, ".coxph"))
+}
+
+
+
 loadProcessWrite <- function (x, ...) {
   UseMethod("loadProcessWrite", x)
 }
